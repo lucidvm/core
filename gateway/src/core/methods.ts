bc20cc8ec5c92564d2ad9bab8ecd18d462aac66f
dither <111248171+dithercat@users.noreply.github.com>
2022-08-19 03:12:21 -0400

    wait, why did i remove this?? too sleepy to be making commits

--- gateway/src/core/methods.ts
+++ gateway/src/core/methods.ts
@@ -87,6 +87,9 @@ export const defaultMethods: {
             }
             ctx.sendRename(nick, RENAME_OK);
             ctx.gw.sendExclude(ctx.channel, ctx, "rename", true, ctx.nick, nick);
+
+            // workaround for vanilla 1.2 frontend bug related to losing visible rank on rename
+            ctx.send("adduser", 1, nick, ctx.rank);
         }
         const oldnick = ctx.nick;
         ctx.nick = nick;
